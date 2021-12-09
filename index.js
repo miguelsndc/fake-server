d861f00b82321c41d6ce3bcf950fc21219e8a643
miguelsndc <mglsndc@gmail.com>
2021-12-09 15:26:08 -0300

    fake server

---
+++ index.js
@@ -0,0 +1,26 @@
+const faker = require('faker');
+const fs = require('fs');
+
+const generatePersonsFakeData = amount => {
+  const persons = [];
+
+  while (amount >= 0) {
+    persons.push({
+      id: amount,
+      name: faker.name.firstName(),
+      description: faker.lorem.paragraphs(2),
+      picture: faker.image.avatar(),
+      country: faker.address.country(),
+      joining_date: faker.date.future(),
+    });
+
+    amount--;
+  }
+
+  return persons;
+};
+
+fs.writeFileSync(
+  'db.json',
+  JSON.stringify({ users: generatePersonsFakeData(20) })
+);
---
+++ mock-server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+node index.js
+yarn json-server --watch db.json --port=5000
